d44fc2ff8bd28ed46e84401e9f8f6639e15fd30d
Evgenii Seliavka <eseliavka@griddynamics.com>
2016-06-21 13:59:00 +0300

    move getJobJson.gnuplot to separate repo + tests for lru cache

--- lru_cache.py
+++ lru_cache.py
@@ -1,10 +1,19 @@
-"""Simple LRU cache implementation in python"""
+"""
+Simple LRU cache implementation in python
+"""
+
+import unittest
+import random
+
 class LRUCache(object):
-    """Class which implement LRU cache algorithm"""
+    """
+    Class which implement LRU cache algorithm
+    """
 
     def __init__(self, capacity):
         """
         :type capacity: int
+        :rtype: None
         """
         self.capacity = capacity
         self.cache = dict()
@@ -13,6 +22,7 @@ class LRUCache(object):
 
     def get(self, key):
         """
+        :type key: str
         :rtype: int
         """
 
@@ -41,3 +51,28 @@ class LRUCache(object):
         else:
             self.hash_table[key] += 1
             self.cache[key] = value
+
+class TestLRUCache(unittest.TestCase):
+    """
+    Test for LRUCache
+    """
+
+    def setUp(self):
+        self.lru_cache = LRUCache(10)
+
+    def test_get_no_values(self):
+        """
+        No values in LRU cache
+        """
+        self.assertEqual(self.lru_cache.get(50), -1)
+
+    def test_set_values(self):
+        """
+        Set values to hash
+        """
+        for i in range(10):
+            self.lru_cache.set(i, i+10)
+        self.assertEqual(len(self.lru_cache.cache), 10)
+
+if __name__ == '__main__':
+    unittest.main()
