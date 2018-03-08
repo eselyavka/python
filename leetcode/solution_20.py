8cd101c7a884be4fb22c611fabde6bd3d28929c6
Evgenii Seliavka <evg.selyavka@gmail.com>
2018-03-08 16:12:36 -0500

    assigment #20 revisit

--- leetcode/solution_20.py
+++ leetcode/solution_20.py
@@ -30,6 +30,33 @@ class Solution(object):
 
         return not stack
 
+    def isValid2(self, s):
+        def matches(open_par, close_par):
+            _open = '({['
+            closing = ')}]'
+            return _open.index(open_par) == closing.index(close_par)
+
+        balanced = True
+        idx = 0
+        stack = []
+
+        while idx < len(s) and balanced:
+            if s[idx] in '({[':
+                stack.append(s[idx])
+            else:
+                if not stack:
+                    balanced = False
+                else:
+                    r = stack.pop()
+                    if not matches(r, s[idx]):
+                        balanced = False
+            idx += 1
+
+        if not balanced or stack:
+            return False
+
+        return True
+
 class TestSolution(unittest.TestCase):
 
     def test_isValid(self):
@@ -39,12 +66,28 @@ class TestSolution(unittest.TestCase):
         invalid2 = ['(', ']']
         invalid3 = ['(((', ']]]']
         invalid4 = ['(((]', '(]]]']
+        valid2 = ['(asds234)[kk{lll}120]', '{pppp}']
 
         self.assertTrue(all([solution.isValid(s) for s in valid]))
         self.assertFalse(any([solution.isValid(s) for s in invalid]))
         self.assertFalse(any([solution.isValid(s) for s in invalid2]))
         self.assertFalse(any([solution.isValid(s) for s in invalid3]))
         self.assertFalse(any([solution.isValid(s) for s in invalid4]))
+        self.assertTrue(all([solution.isValid(s) for s in valid2]))
+
+    def test_isValid2(self):
+        solution = Solution()
+        valid = ['()', '[(){}]']
+        invalid = ['(]', '([)]']
+        invalid2 = ['(', ']']
+        invalid3 = ['(((', ']]]']
+        invalid4 = ['(((]', '(]]]']
+
+        self.assertTrue(all([solution.isValid2(s) for s in valid]))
+        self.assertFalse(any([solution.isValid2(s) for s in invalid]))
+        self.assertFalse(any([solution.isValid2(s) for s in invalid2]))
+        self.assertFalse(any([solution.isValid2(s) for s in invalid3]))
+        self.assertFalse(any([solution.isValid2(s) for s in invalid4]))
 
 if __name__ == '__main__':
     unittest.main()
