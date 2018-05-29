f68801235d432fa65da60bc67fcef781005e90de
Evgenii Seliavka <evg.selyavka@gmail.com>
2018-05-29 12:28:14 -0400

    assigment #49

--- leetcode/solution_49.py
+++ leetcode/solution_49.py
@@ -8,28 +8,28 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-
-        res = dict()
-
+        d = {}
         for s in strs:
-            sorted_str = ''.join(sorted(s))
+            ss = ''.join(sorted(s))
+            try:
+                d[ss].append(s)
+            except KeyError:
+                d[ss] = [s]
 
-            if res.has_key(sorted_str):
-                res[sorted_str].append(s)
-            else:
-                res[sorted_str] = [s]
-
-        return [sorted(mas) for mas in res.values()]
+        return d.values()
 
 class TestSolution(unittest.TestCase):
-
     def test_groupAnagrams(self):
-        anagrams = ["eat", "tea", "tan", "ate", "nat", "bat"]
         solution = Solution()
-        self.assertEqual(solution.groupAnagrams(anagrams),
-                         [["nat", "tan"],
-                          ["bat"],
-                          ["ate", "eat", "tea"]])
+        self.assertEqual(solution.groupAnagrams(["eat",
+                                                 "tea",
+                                                 "tan",
+                                                 "ate",
+                                                 "nat",
+                                                 "bat"]),
+                         [['tan', 'nat'],
+                          ['bat'],
+                          ['eat', 'tea', 'ate']])
 
 if __name__ == '__main__':
     unittest.main()
