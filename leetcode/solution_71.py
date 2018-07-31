68138b36bf0117163a7c70358b0961083cc29182
Evgenii Seliavka <evg.selyavka@gmail.com>
2018-07-31 17:30:56 -0400

    assigment #71

---
+++ leetcode/solution_71.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import unittest
+
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        tokens = path.split('/')
+        res = []
+        i, j = len(tokens) - 1 if tokens[len(tokens)-1] != '' else len(tokens) - 2, 0
+
+        while j <= i:
+            if tokens[j] in ['.', '']:
+                j += 1
+                continue
+
+            if tokens[j] == '..':
+                try:
+                    res.pop()
+                except IndexError:
+                    pass
+                j += 1
+                continue
+
+            res.append(tokens[j])
+            j += 1
+
+        return '/' + '/'.join(res)
+
+class TestSolution(unittest.TestCase):
+    def test_simplifyPath(self):
+        solution = Solution()
+        self.assertEqual(solution.simplifyPath('/home/'), '/home')
+        self.assertEqual(solution.simplifyPath('/a/./b/../../c/'), '/c')
+        self.assertEqual(solution.simplifyPath('/home//foo/'), '/home/foo')
+        self.assertEqual(solution.simplifyPath('/../'), '/')
+        self.assertEqual(solution.simplifyPath('/'), '/')
+        self.assertEqual(solution.simplifyPath('///'), '/')
+        self.assertEqual(solution.simplifyPath('/..'), '/')
+
+if __name__ == '__main__':
+    unittest.main()
