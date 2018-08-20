87bcb3fabb88de9654641e1174e7a1c08134ee78
Evgenii Seliavka <evg.selyavka@gmail.com>
2018-08-20 14:54:28 -0400

    assigment #203

---
+++ leetcode/solution_203.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import unittest
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        prev = None
+        _head = head
+
+        while head:
+            if head.val == val:
+                if prev:
+                    prev.next = head.next
+                else:
+                    _head = head.next
+            else:
+                prev = head
+
+            head = head.next
+        return _head
+
+class TestSolution(unittest.TestCase):
+
+    def list_to_list(self, head):
+        actual = []
+
+        while head is not None:
+            actual += [head.val]
+            head = head.next
+
+        return actual
+
+    def test_removeElements(self):
+        solution = Solution()
+
+        lst = ListNode(1)
+        lst.next = ListNode(2)
+        lst.next.next = ListNode(6)
+        lst.next.next.next = ListNode(3)
+        lst.next.next.next.next = ListNode(4)
+        lst.next.next.next.next.next = ListNode(5)
+        lst.next.next.next.next.next.next = ListNode(6)
+
+        actual = self.list_to_list(solution.removeElements(lst, 6))
+        expected = [1, 2, 3, 4, 5]
+        self.assertEqual(actual, expected)
+
+        lst = ListNode(1)
+        lst.next = ListNode(1)
+        lst.next.next = ListNode(6)
+        lst.next.next.next = ListNode(3)
+        lst.next.next.next.next = ListNode(4)
+        lst.next.next.next.next.next = ListNode(5)
+        lst.next.next.next.next.next.next = ListNode(6)
+
+        actual = self.list_to_list(solution.removeElements(lst, 1))
+        expected = [6, 3, 4, 5, 6]
+        self.assertEqual(actual, expected)
+
+        lst = ListNode(1)
+
+        actual = self.list_to_list(solution.removeElements(lst, 1))
+        expected = []
+        self.assertEqual(actual, expected)
+
+        lst = ListNode(1)
+        lst.next = ListNode(1)
+        lst.next.next = ListNode(1)
+
+        actual = self.list_to_list(solution.removeElements(lst, 1))
+        expected = []
+        self.assertEqual(actual, expected)
+
+        lst = ListNode(1)
+        lst.next = ListNode(2)
+        lst.next.next = ListNode(2)
+        lst.next.next.next = ListNode(1)
+
+        actual = self.list_to_list(solution.removeElements(lst, 2))
+        expected = [1, 1]
+        self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
