ed1f29c7811ae1812a6173e53bcfd83875dd8559
Evgenii Seliavka <eseliavka@twitter.com>
2019-02-02 21:29:48 -0800

    assigment #856

---
+++ leetcode/solution_856.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest
+
+class Solution(object):
+    def scoreOfParentheses(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        if not S:
+            return 0
+
+        stack = [0]
+        for c in S:
+            if c == '(':
+                stack.append(0)
+            else:
+                depth = stack.pop()
+                stack[-1] += max(2*depth, 1)
+
+        return stack[-1]
+
+
+class TestSolution(unittest.TestCase):
+    def test_scoreOfParentheses(self):
+        solution = Solution()
+        self.assertEqual(solution.scoreOfParentheses('()'), 1)
+        self.assertEqual(solution.scoreOfParentheses('(())'), 2)
+        self.assertEqual(solution.scoreOfParentheses('()()'), 2)
+        self.assertEqual(solution.scoreOfParentheses('(()(()))'), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
