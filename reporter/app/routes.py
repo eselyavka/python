f530805041431d5b0d4c4cd82222da5bc1117abe
Evgenii Seliavka <eseliavka@twitter.com>
2019-04-02 11:50:16 -0700

    on every 10th request return 0

--- reporter/app/routes.py
+++ reporter/app/routes.py
@@ -4,20 +4,24 @@ from flask import jsonify, request
 
 from app import app
 
+idx = 0
 
 @app.route('/')
 @app.route('/status')
 def index():
+    global idx
     apps = ['db', 'cache', 'web']
     rnd_str = ''
     host = list(request.headers['Host'])
     for _ in host:
         rnd_str += random.choice(host)
-    _hash = hash(rnd_str)
-    random.choice(apps)
+
+    _hash = 0 if idx and idx % 10 == 0 else hash(rnd_str)
 
     def rnd(postfix=None):
         return str(random.randint(1, 9)) + (postfix if postfix else '')
+
+    idx += 1
 
     return jsonify({"Application": random.choice(apps),
                     "Version": rnd('.') + rnd('.') + rnd(),
