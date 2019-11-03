dfaeb6714f4193b44ae7a6ccc42a98d358a1fc0b
Evgenii Seliavka <eseliavka@twitter.com>
2019-11-03 14:42:33 -0800

    revisit #66

--- leetcode/solution_66.py
+++ leetcode/solution_66.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def plusOne(self, digits):
         """
@@ -29,15 +30,52 @@ class Solution(object):
 
         return digits
 
+    def plusOne2(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+        if digits[-1] + 1 < 10:
+            digits[-1] += 1
+            return digits
+        else:
+            pivotal = None
+            for i in range(len(digits) - 1, -1, -1):
+                if digits[i] + 1 < 10:
+                    pivotal = i
+                    break
+
+            if pivotal is None:
+                return [1] + [0] * len(digits)
+
+            digits[pivotal] = digits[pivotal] + 1
+            digits = digits[:pivotal+1] + [0] * (len(digits) - (pivotal + 1))
+
+            return digits
+
+
 class TestSolution(unittest.TestCase):
     def test_plusOne(self):
         solution = Solution()
-        self.assertEquals(solution.plusOne([0]), [1])
-        self.assertEquals(solution.plusOne([1]), [2])
-        self.assertEquals(solution.plusOne([1, 2, 3]), [1, 2, 4])
-        self.assertEquals(solution.plusOne([4, 3, 2, 1]), [4, 3, 2, 2])
-        self.assertEquals(solution.plusOne([9, 9]), [1, 0, 0])
-        self.assertEquals(solution.plusOne([9, 9, 9]), [1, 0, 0, 0])
+        self.assertListEqual(solution.plusOne([0]), [1])
+        self.assertListEqual(solution.plusOne([1]), [2])
+        self.assertListEqual(solution.plusOne([1, 2, 3]), [1, 2, 4])
+        self.assertListEqual(solution.plusOne([4, 3, 2, 1]), [4, 3, 2, 2])
+        self.assertListEqual(solution.plusOne([9, 9]), [1, 0, 0])
+        self.assertListEqual(solution.plusOne([9, 9, 9]), [1, 0, 0, 0])
+
+    def test_plusOne2(self):
+        solution = Solution()
+        self.assertListEqual(solution.plusOne2([9]), [1, 0])
+        self.assertListEqual(solution.plusOne2([9, 9]), [1, 0, 0])
+        self.assertListEqual(solution.plusOne2([3, 9, 9]), [4, 0, 0])
+        self.assertListEqual(solution.plusOne2([4, 3, 9, 9]), [4, 4, 0, 0])
+        self.assertListEqual(solution.plusOne2([0]), [1])
+        self.assertListEqual(solution.plusOne2([1]), [2])
+        self.assertListEqual(solution.plusOne2([1, 2, 3]), [1, 2, 4])
+        self.assertListEqual(solution.plusOne2([4, 3, 2, 1]), [4, 3, 2, 2])
+
 
 if __name__ == '__main__':
     unittest.main()
