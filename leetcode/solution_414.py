90a4c4c4fb4accac63c4e800e383c3d135c38c93
Evgenii Seliavka <eseliavka@twitter.com>
2019-11-14 17:22:46 -0800

    revisit $414

--- leetcode/solution_414.py
+++ leetcode/solution_414.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def thirdMax(self, nums):
         """
@@ -24,6 +25,30 @@ class Solution(object):
 
         return third if third != float('-inf') else _max
 
+    def thirdMax2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        first, second, third = [float('-inf')] * 3
+        i = 0
+
+        while i < len(nums):
+            if nums[i] > first:
+                third = second
+                second = first
+                first = nums[i]
+            elif nums[i] > second and nums[i] != first:
+                third = second
+                second = nums[i]
+            elif nums[i] > third and nums[i] != first and nums[i] != second:
+                third = nums[i]
+
+            i += 1
+
+        return third if third != float('-inf') else first
+
+
 class TestSolution(unittest.TestCase):
 
     def test_thirdMax(self):
@@ -36,6 +61,14 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.thirdMax([1, 0, 1, 1]), 1)
         self.assertEqual(solution.thirdMax([1, 2, 3, 1, 2, 3]), 1)
         self.assertEqual(solution.thirdMax([1, -2147483648, 2]), -2147483648)
+
+        self.assertEqual(solution.thirdMax2([1, 2, 3, 1]), 1)
+        self.assertEqual(solution.thirdMax2([1, 2]), 2)
+        self.assertEqual(solution.thirdMax2([2, 2, 3, 1]), 1)
+        self.assertEqual(solution.thirdMax2([1, 0, 1, 1]), 1)
+        self.assertEqual(solution.thirdMax2([1, 2, 3, 1, 2, 3]), 1)
+        self.assertEqual(solution.thirdMax2([1, -2147483648, 2]), -2147483648)
+
 
 if __name__ == '__main__':
     unittest.main()
