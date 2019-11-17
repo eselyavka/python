f416ee0e6f8b885598321290270b89ce1630da5b
Evgenii Seliavka <eseliavka@twitter.com>
2019-11-16 18:39:25 -0800

    revisit 674

--- leetcode/solution_674.py
+++ leetcode/solution_674.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def findLengthOfLCIS(self, nums):
         """
@@ -24,6 +25,32 @@ class Solution(object):
 
         return max(cnt, buf)
 
+    def findLengthOfLCIS2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if not nums:
+            return 0
+
+        if nums == 1:
+            return 1
+
+        n = len(nums)
+        i, length_so_far = 1, 1
+        max_ = 0
+        while i < n:
+            if nums[i-1] < nums[i]:
+                length_so_far += 1
+            else:
+                max_ = max(max_, length_so_far)
+                length_so_far = 1
+            i += 1
+
+        return max(max_, length_so_far)
+
+
 class TestSolution(unittest.TestCase):
     def test_findLengthOfLCIS(self):
         solution = Solution()
@@ -32,6 +59,13 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.findLengthOfLCIS([1, 3, 2, 1, 7]), 2)
         self.assertEqual(solution.findLengthOfLCIS([1, 3, 5, 7]), 4)
         self.assertEqual(solution.findLengthOfLCIS([1, 3, 5, 4, 2, 3, 4, 5]), 4)
+
+        self.assertEqual(solution.findLengthOfLCIS2([1, 3, 5, 4, 7]), 3)
+        self.assertEqual(solution.findLengthOfLCIS2([2, 2, 2, 2, 2]), 1)
+        self.assertEqual(solution.findLengthOfLCIS2([1, 3, 2, 1, 7]), 2)
+        self.assertEqual(solution.findLengthOfLCIS2([1, 3, 5, 7]), 4)
+        self.assertEqual(solution.findLengthOfLCIS2([1, 3, 5, 4, 2, 3, 4, 5]), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
