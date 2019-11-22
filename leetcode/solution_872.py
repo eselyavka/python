6d14eda1d3a5a41dced357b2f45ff3d35ac6290c
Evgenii Seliavka <eseliavka@twitter.com>
2019-11-21 16:01:15 -0800

    revisit #872

--- leetcode/solution_872.py
+++ leetcode/solution_872.py
@@ -2,11 +2,13 @@
 
 import unittest
 
+
 class TreeNode(object):
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
+
 
 class Solution(object):
     def leafSimilar(self, root1, root2):
@@ -17,6 +19,7 @@ class Solution(object):
         """
         res1 = []
         res2 = []
+
         def _rec(root, arr):
             if not root:
                 return
@@ -31,6 +34,33 @@ class Solution(object):
         _rec(root2, res2)
 
         return res1 == res2
+
+
+class Solution2(object):
+    def traverse(self, root):
+        stack = [root]
+        leaf_nodes = list()
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            if not (node.right or node.left):
+                leaf_nodes.append(node.val)
+        return leaf_nodes
+
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        leaf_nodes1 = self.traverse(root1)
+        leaf_nodes2 = self.traverse(root2)
+
+        return leaf_nodes1 == leaf_nodes2 if len(leaf_nodes1) == len(leaf_nodes1) else False
+
 
 class TestSolution(unittest.TestCase):
 
@@ -48,8 +78,11 @@ class TestSolution(unittest.TestCase):
         root2.right.right = TreeNode(6)
 
         solution = Solution()
-
         self.assertTrue(solution.leafSimilar(root, root2))
+
+        solution2 = Solution2()
+        self.assertTrue(solution2.leafSimilar(root, root2))
+
 
 if __name__ == '__main__':
     unittest.main()
