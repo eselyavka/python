64094132ee3ab22b481755c011a5b95413f7b71f
Evgenii Seliavka <eseliavka@twitter.com>
2019-11-24 18:18:41 -0800

    revisit #118

--- leetcode/solution_118.py
+++ leetcode/solution_118.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def generate(self, numRows):
         """
@@ -26,12 +27,36 @@ class Solution(object):
             arr.append(arr[i]*(rowIndex-i)/(i+1))
         return arr
 
+
+class Solution2(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        matrix = [[1]*(i+1) for i in range(numRows)]
+
+        for i in range(numRows):
+            for j in range(1, i):
+                matrix[i][j] = matrix[i-1][j-1] + matrix[i-1][j]
+
+        return matrix
+
+
 class TestSolution(unittest.TestCase):
     def test_generate(self):
         solution = Solution()
-        self.assertEqual(solution.generate(5), [[1], [1, 1],
+        self.assertEqual(solution.generate(5), [[1],
+                                                [1, 1],
                                                 [1, 2, 1], [1, 3, 3, 1],
                                                 [1, 4, 6, 4, 1]])
+        solution2 = Solution2()
+        self.assertEqual(solution2.generate(5), [[1],
+                                                 [1, 1],
+                                                 [1, 2, 1],
+                                                 [2, 3, 3, 1],
+                                                 [1, 4, 6, 4, 1]])
+
 
 if __name__ == '__main__':
     unittest.main()
