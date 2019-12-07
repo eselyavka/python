c64042eaca143d7c347fb016f46a8cf1362e9900
Evgenii Seliavka <eseliavka@twitter.com>
2019-12-06 23:48:47 -0800

    revisit #200

--- leetcode/solution_200.py
+++ leetcode/solution_200.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def dfs(self, grid, seen, row, col):
         if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[row]):
@@ -35,6 +36,54 @@ class Solution(object):
 
         return islands
 
+
+class Solution2(object):
+    def find(self, arr, x):
+        if arr[x] == x:
+            return x
+        arr[x] = self.find(arr, arr[x])
+        return arr[x]
+
+    def union(self, arr, rank, x, y):
+        xx = self.find(arr, x)
+        yy = self.find(arr, y)
+        if xx != yy:
+            if rank[xx] > rank[yy]:
+                arr[yy] = arr[xx]
+            elif rank[yy] > rank[xx]:
+                arr[xx] = arr[yy]
+            else:
+                arr[yy] = arr[xx]
+                rank[xx] += 1
+
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+
+        n = len(grid)
+        m = len(grid[0])
+        arr = [i for i in range(n*m)]
+        rank = [0 for _ in range(n*m)]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    if i > 0 and grid[i-1][j] == '1':
+                        self.union(arr, rank, i * m + j, (i - 1) * m + j)
+                    if j > 0 and grid[i][j - 1] == '1':
+                        self.union(arr, rank, i * m + j, i * m + (j - 1))
+        s = set()
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    id_ = self.find(arr, i*m+j)
+                    s.add(id_)
+        return len(s)
+
+
 class TestSolution(unittest.TestCase):
     def test_numIslands(self):
         solution = Solution()
@@ -42,6 +91,12 @@ class TestSolution(unittest.TestCase):
                                               ["1", "1", "0", "0", "0"],
                                               ["0", "0", "1", "0", "0"],
                                               ["0", "0", "0", "1", "1"]]), 3)
+        solution2 = Solution2()
+        self.assertEqual(solution2.numIslands([["1", "1", "0", "0", "0"],
+                                               ["1", "1", "0", "0", "0"],
+                                               ["0", "0", "1", "0", "0"],
+                                               ["0", "0", "0", "1", "1"]]), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
