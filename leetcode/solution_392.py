951656796ccbbddc98eb223ccf1f7981d7046fb2
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-01-11 13:36:48 -0800

    revisit #392

--- leetcode/solution_392.py
+++ leetcode/solution_392.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def isSubsequence(self, s, t):
         """
@@ -15,20 +16,19 @@ class Solution(object):
         if len(s) > len(t):
             return False
 
-        i = len(s) - 1
-        j = len(t) - 1
+        i, j = 0, 0
 
-        while i > -1:
-            if s[i] == t[j]:
-                i -= 1
-                j -= 1
-            else:
-                if j > -1:
-                    j -= 1
+        while i < len(s):
+            try:
+                if s[i] == t[j]:
+                    i += 1
+                    j += 1
                 else:
-                    return False
+                    j += 1
+            except IndexError:
+                return False
 
-        if i > 0:
+        if i != len(s):
             return False
 
         return True
@@ -40,6 +40,7 @@ class TestSolution(unittest.TestCase):
         self.assertTrue(solution.isSubsequence("abc", "ahbgdc"))
         self.assertFalse(solution.isSubsequence("axc", "ahbgdc"))
         self.assertTrue(solution.isSubsequence("rrr", "kkkkrrr"))
+
 
 if __name__ == '__main__':
     unittest.main()
