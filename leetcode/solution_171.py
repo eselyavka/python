11a560fcb968149522270e52233d3c4cdd1b34dc
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-01-20 14:17:20 -0800

    revisit #171

--- leetcode/solution_171.py
+++ leetcode/solution_171.py
@@ -3,13 +3,14 @@
 import unittest
 from string import ascii_uppercase
 
+
 class Solution(object):
     def titleToNumber(self, s):
         """
         :type s: str
         :rtype: int
         """
-        d = {ascii_uppercase[x]:x+1 for x in range(len(ascii_uppercase))}
+        d = {ascii_uppercase[x]: x+1 for x in range(len(ascii_uppercase))}
 
         if s in d:
             return d[s]
@@ -24,6 +25,16 @@ class Solution(object):
 
         return res
 
+
+class Solution2(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return reduce(lambda res, c: res * 26 + ord(c) - ord('A') + 1, s, 0)
+
+
 class TestSolution(unittest.TestCase):
 
     def test_titleToNumber(self):
@@ -31,6 +42,12 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.titleToNumber('A'), 1)
         self.assertEqual(solution.titleToNumber('AB'), 28)
         self.assertEqual(solution.titleToNumber('ZY'), 701)
+
+        solution2 = Solution2()
+        self.assertEqual(solution2.titleToNumber('A'), 1)
+        self.assertEqual(solution2.titleToNumber('AB'), 28)
+        self.assertEqual(solution2.titleToNumber('ZY'), 701)
+
 
 if __name__ == '__main__':
     unittest.main()
