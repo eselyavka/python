456ff77331b838ffd0be8d3c4046c8560814ea19
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-03-27 16:10:10 -0700

    revisit #1379

--- leetcode/solution_1379.py
+++ leetcode/solution_1379.py
@@ -18,21 +18,36 @@ class Solution(object):
         :type target: TreeNode
         :rtype: TreeNode
         """
-        s = [cloned]
+        def hasPath(root, path, x, direction):
+            if not root:
+                return False
 
-        while s:
-            cnt = len(s)
-            while cnt:
-                node = s.pop()
-                if node.val == target.val:
-                    return node
+            if direction is not None:
+                path.append(direction)
 
-                if node.left:
-                    s.append(node.left)
-                if node.right:
-                    s.append(node.right)
+            if id(root) == id(x):
+                return True
 
-                cnt -= 1
+            if hasPath(root.left, path, x, True) or hasPath(root.right, path, x, False):
+                return True
+
+            path.pop(-1)
+
+            return False
+
+        path = []
+        hasPath(original, path, target, None)
+
+        def traverse(root, path, k):
+            if k >= len(path):
+                return root
+
+            if path[k]:
+                return traverse(root.left, path, k+1)
+
+            return traverse(root.right, path, k+1)
+
+        return traverse(cloned, path, 0)
 
 
 class TestSolution(unittest.TestCase):
