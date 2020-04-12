d073948e4e89ac595797c881a6b2f9f8c0116825
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-04-12 11:39:18 -0700

    implementation of the Rabin–Karp algorithm

---
+++ rabin_karp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import functools
+
+
+def rabin_karp(t, s):
+    if len(s) > len(t):
+        return -1
+
+    base = 26
+
+    t_hash = functools.reduce(lambda h, c: h * base + ord(c), t[:len(s)], 0)
+    s_hash = functools.reduce(lambda h, c: h * base + ord(c), s, 0)
+    power_s = base ** max(len(s) - 1, 0)
+
+    for i in range(len(s), len(t)):
+        if t_hash == s_hash and t[i - len(s):i] == s:
+            return i - len(s)
+
+        t_hash -= ord(t[i - len(s)]) * power_s
+        t_hash = t_hash * base + ord(t[i])
+
+        if t_hash == s_hash and t[-len(s):] == s:
+            return len(t) - len(s)
+
+    return -1
+
+
+if __name__ == '__main__':
+    assert rabin_karp('GACGCCA', 'CGC') == 2
