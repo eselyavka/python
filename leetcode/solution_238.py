9c8a8f631af9c21bb209c3914d9db8cd8ea14bed
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-04-27 13:41:49 -0700

    revisit #238

--- leetcode/solution_238.py
+++ leetcode/solution_238.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
@@ -18,7 +19,33 @@ class Solution(object):
         for i in range(len(nums)-1, -1, -1):
             res[i] = res[i] * p
             p = p*nums[i]
+
         return res
+
+    def productExceptSelf2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left, right = [], []
+        L, R = 1, 1
+
+        for num in nums:
+            left.append(L)
+            L = L * num
+
+        for i in range(len(nums) - 1, -1, -1):
+            right.append(R)
+            R = R * nums[i]
+
+        res = []
+        j = len(right) - 1
+        for num in left:
+            res.append(num * right[j])
+            j -= 1
+
+        return res
+
 
 class TestSolution(unittest.TestCase):
 
@@ -30,6 +57,11 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.productExceptSelf(nums), [24, 12, 8, 6])
         self.assertEqual(solution.productExceptSelf(nums2), [0, 0])
         self.assertEqual(solution.productExceptSelf(nums3), [20, 0, 0])
+
+        self.assertEqual(solution.productExceptSelf2(nums), [24, 12, 8, 6])
+        self.assertEqual(solution.productExceptSelf2(nums2), [0, 0])
+        self.assertEqual(solution.productExceptSelf2(nums3), [20, 0, 0])
+
 
 if __name__ == '__main__':
     unittest.main()
