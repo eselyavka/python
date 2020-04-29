4666e26078a1e4858b3875b81c57961aec39201c
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-04-29 12:40:34 -0700

    revisit #415

--- leetcode/solution_415.py
+++ leetcode/solution_415.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def addStrings(self, num1, num2):
         """
@@ -9,25 +10,33 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        def atoi(s):
+            res, i = 0, 0
+            for c in reversed(s):
+                res += int(c) * (10 ** i)
+                i += 1
 
-        d = {len(num1) - i:pow(10, i - 1) for i in range(len(num1), 0, -1)}
-        d2 = {len(num2) - i:pow(10, i - 1) for i in range(len(num2), 0, -1)}
+            return res
 
-        res = 0
+        def itoa(num):
+            s = []
+            while True:
+                s.append(chr(ord('0') + num % 10))
+                num //= 10
+                if num == 0:
+                    break
 
-        for i, c in enumerate(num1):
-            res += d[i] * int(c)
+            return ''.join(reversed(s))
 
-        for i, c in enumerate(num2):
-            res += d2[i] * int(c)
+        return itoa(atoi(num1) + atoi(num2))
 
-        return str(res)
 
 class TestSolution(unittest.TestCase):
 
     def test_addStrings(self):
         solution = Solution()
         self.assertEqual(solution.addStrings('124', '34'), '158')
+
 
 if __name__ == '__main__':
     unittest.main()
