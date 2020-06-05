78100950335588312a09b32f0f436343a50eae58
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-06-05 15:21:05 -0700

    revisit #278

--- leetcode/solution_278.py
+++ leetcode/solution_278.py
@@ -2,38 +2,39 @@
 
 import unittest
 
-def isBadVersion(version):
-    if version > 1702766718:
-        return True
-    return False
+
+def isBadVersion(mid):
+    return mid == 4
+
 
 class Solution(object):
-    def binary_search(self, l, r):
-        while l <= r:
-            mid = l + (r - l) / 2
-            if not isBadVersion(mid):
-                l = mid + 1
-            elif isBadVersion(mid) and not isBadVersion(mid - 1):
-                return mid
-            else:
-                r = mid - 1
-
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return n if isBadVersion(n) else None
+        left, right = 1, n
 
-        return self.binary_search(1, n)
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                if isBadVersion(mid-1):
+                    right = mid - 1
+                else:
+                    return mid
+            else:
+                if isBadVersion(mid+1):
+                    return mid + 1
+                else:
+                    left = mid + 1
+
 
 class TestSolution(unittest.TestCase):
 
     def test_firstBadVersion(self):
         solution = Solution()
-        self.assertEqual(solution.firstBadVersion(1), None)
-        self.assertEqual(solution.firstBadVersion(2126753390), 1702766719)
+        self.assertEqual(solution.firstBadVersion(5), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
