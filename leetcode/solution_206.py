9060a7ebd8ace5163c5205bf319800299c6c11d5
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-06-13 11:03:46 -0700

    revisit #206

--- leetcode/solution_206.py
+++ leetcode/solution_206.py
@@ -2,10 +2,12 @@
 
 import unittest
 
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution(object):
     def reverseList(self, head):
@@ -14,7 +16,7 @@ class Solution(object):
         :rtype: ListNode
         """
         if not head:
-            return
+            return None
 
         stack = []
         while head:
@@ -31,16 +33,49 @@ class Solution(object):
 
         return rev
 
+
+class Solution2(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+
+        prev = None
+        curr = head
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        return prev
+
+
 class TestSolution(unittest.TestCase):
 
     def test_reverseList(self):
         solution = Solution()
+
         head = ListNode(1)
         head.next = ListNode(2)
         head.next.next = ListNode(3)
-        head2 = ListNode(10)
-        self.assertTrue(isinstance(solution.reverseList(head), ListNode))
-        self.assertTrue(isinstance(solution.reverseList(head2), ListNode))
+
+        rev = solution.reverseList(head)
+
+        self.assertTrue(rev.val, 3)
+        self.assertTrue(rev.next.val, 2)
+        self.assertTrue(rev.next.next.val, 1)
+
+        solution2 = Solution2()
+        rev = solution2.reverseList(head)
+
+        self.assertTrue(rev.val, 1)
+        self.assertTrue(rev.next.val, 2)
+        self.assertTrue(rev.next.next.val, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
