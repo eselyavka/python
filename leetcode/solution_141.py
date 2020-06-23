be1acac0cfaf187905cef228df497b727fbfd931
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-06-23 15:30:31 -0700

    revisit #141

--- leetcode/solution_141.py
+++ leetcode/solution_141.py
@@ -2,10 +2,12 @@
 
 import unittest
 
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution(object):
     def hasCycle(self, head):
@@ -23,6 +25,23 @@ class Solution(object):
             head = head.next
 
         return False
+
+
+class Solution2(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+
+        while slow and fast.next and fast.next.next:
+            slow, fast = slow.next, fast.next.next
+            if slow is fast:
+                return True
+
+        return False
+
 
 class TestSolution(unittest.TestCase):
     def test_hasCycle(self):
@@ -49,6 +68,10 @@ class TestSolution(unittest.TestCase):
         self.assertFalse(solution.hasCycle(lst))
         self.assertFalse(solution.hasCycle(lst2))
         self.assertTrue(solution.hasCycle(lst3))
+
+        solution2 = Solution2()
+        self.assertTrue(solution2.hasCycle(lst3))
+
 
 if __name__ == '__main__':
     unittest.main()
