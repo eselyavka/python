fda57d60956aab2bcf6309b28cf524bba7744641
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-07-16 18:34:01 -0700

    assigment #777

--- leetcode/solution_777.py
+++ leetcode/solution_777.py
@@ -13,51 +13,44 @@ class Solution(object):
         if len(start) != len(end):
             return False
 
-        if len(start) == 1:
-            return start == end
+        if start.replace('X', '') != end.replace('X', ''):
+            return False
 
-        n = len(start)
-        i = 0
-        res = []
-        ss = list(start)
-        ee = list(end)
-        while i < n - 1:
-            if ss[i] != ee[i]:
-                if ss[i] == 'R' and ss[i+1] == 'X':
-                    ss[i], ss[i+1] = ss[i+1], ss[i]
-                elif ss[i] == 'X' and ss[i+1] == 'L':
-                    ss[i], ss[i+1] = ss[i+1], ss[i]
-                else:
-                    if ss[i] == 'X':
-                        j = i
-                        while j < n - 1:
-                            if ss[j] == 'L':
-                                ss[i], ss[j] = ss[j], ss[i]
-                                break
-                            j += 1
-                    elif ss[i] == 'R':
-                        # R first
-                        j = i
-                        while j < n - 1:
-                            if ss[j] == 'X':
-                                ss[i], s[j] = ss[j], ss[i]
-                                break
-                            j += 1
-                    else:
+        used = [False] * len(end)
+        for i, c in enumerate(end):
+            if c == 'L':
+                found = False
+                for j in range(i, len(start)):
+                    if start[j] == 'R':
                         return False
-            i += 1
-        return ss == ee
+                    if start[j] == 'L' and not used[j]:
+                        used[j] = True
+                        found = True
+                        break
+
+                if not found:
+                    return False
+
+            if c == 'R':
+                found = False
+                for j in range(i, -1, -1):
+                    if start[j] == 'L':
+                        return False
+                    if start[j] == 'R' and not used[j]:
+                        used[j] = True
+                        found = True
+                        break
+
+                if not found:
+                    return False
+
+        return True
 
 
 class TestSolution(unittest.TestCase):
     def test_canTransform(self):
         solution = Solution()
-        self.assertFalse(solution.canTransform('L', 'X'))
         self.assertTrue(solution.canTransform('RXXLRXRXL', 'XRLXXRRLX'))
-        self.assertFalse(solution.canTransform('RL', 'LR'))
-        self.assertFalse(solution.canTransform('XXRXXLXXXX', 'XXXXRXXLXX'))
-        self.assertTrue(solution.canTransform('XXXXXLXXXX', 'LXXXXXXXXX'))
-        self.assertTrue(solution.canTransform('XLXRRXXRXX', 'LXXXXXXRRR'))
 
 
 if __name__ == '__main__':
