b5b4d19ed10703d819d1d24b34f8b97e2463f456
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-07-26 14:21:25 -0700

    revisit #1299

--- leetcode/solution_1299.py
+++ leetcode/solution_1299.py
@@ -38,10 +38,37 @@ class Solution(object):
         return arr
 
 
+class Solution2(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        if len(arr) == 1:
+            arr[0] = -1
+            return arr
+
+        if len(arr) == 2:
+            arr[0] = arr[1]
+            arr[1] = -1
+            return arr
+
+        max_so_far = arr[-1]
+        for i in range(len(arr)-2, -1, -1):
+            buf = max(arr[i], max_so_far)
+            arr[i] = max_so_far
+            max_so_far = max(max_so_far, buf)
+        arr[-1] = -1
+        return arr
+
+
 class TestSolution(unittest.TestCase):
     def test_replaceElements(self):
         solution = Solution()
         self.assertListEqual(solution.replaceElements([17, 18, 5, 4, 6, 1]), [18, 6, 6, 6, 1, -1])
+
+        solution2 = Solution2()
+        self.assertListEqual(solution2.replaceElements([17, 18, 5, 4, 6, 1]), [18, 6, 6, 6, 1, -1])
 
 
 if __name__ == '__main__':
