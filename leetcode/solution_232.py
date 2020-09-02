847e67adcb9a590053aaf39d229ee8b22d802596
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-09-02 16:46:01 -0700

    revisit #232

--- leetcode/solution_232.py
+++ leetcode/solution_232.py
@@ -2,44 +2,53 @@
 
 import unittest
 
+
 class MyQueue(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.queue = []
+        self._enq = []
+        self._dec = []
 
     def push(self, x):
         """
         Push element x to the back of queue.
         :type x: int
-        :rtype: void
+        :rtype: None
         """
-        self.queue.append(x)
+        self._enq.append(x)
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
         :rtype: int
         """
-        element = self.queue[0]
-        self.queue = self.queue[1:]
-        return element
+        if not self._dec:
+            while self._enq:
+                self._dec.append(self._enq.pop())
+
+        return self._dec.pop()
 
     def peek(self):
         """
         Get the front element.
         :rtype: int
         """
-        return self.queue[0]
+        if not self._dec:
+            while self._enq:
+                self._dec.append(self._enq.pop())
+
+        return self._dec[-1]
 
     def empty(self):
         """
         Returns whether the queue is empty.
         :rtype: bool
         """
-        return len(self.queue) == 0
+        return all([len(self._enq) == 0, len(self._dec) == 0])
+
 
 class TestMyQueue(unittest.TestCase):
 
@@ -47,6 +56,7 @@ class TestMyQueue(unittest.TestCase):
         solution = MyQueue()
         for x in range(1, 4):
             solution.push(x)
+
         self.assertEqual(solution.pop(), 1)
         self.assertEqual(solution.peek(), 2)
         self.assertFalse(solution.empty())
@@ -55,6 +65,7 @@ class TestMyQueue(unittest.TestCase):
         self.assertFalse(solution.empty())
         self.assertEqual(solution.pop(), 3)
         self.assertTrue(solution.empty())
+
 
 if __name__ == '__main__':
     unittest.main()
