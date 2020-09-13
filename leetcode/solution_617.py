75de712beaea20f1a2644fb3a75b14de99864255
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-09-12 17:12:09 -0700

    revisit #617

--- leetcode/solution_617.py
+++ leetcode/solution_617.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+
 class TreeNode(object):
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
+
 
 class Solution(object):
     def mergeTrees(self, t1, t2):
@@ -25,11 +27,37 @@ class Solution(object):
         t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
 
+
+class Solution2(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+
+        def rec(t1, t2):
+            if t1 and t2:
+                t1.val += t2.val
+            else:
+                return t1 or t2
+
+            t1.left = rec(t1.left, t2.left)
+            t1.right = rec(t1.right, t2.right)
+
+            return t1
+
+        res = rec(t1, t2)
+
+        return res
+
+
 def print_preorder(root):
     if root:
         print(root.val),
         print_preorder(root.left)
         print_preorder(root.right)
+
 
 if __name__ == '__main__':
 
@@ -48,3 +76,6 @@ if __name__ == '__main__':
 
     merged = Solution().mergeTrees(root, root2)
     print_preorder(merged)
+
+    merged2 = Solution2().mergeTrees(root, root2)
+    print_preorder(merged2)
