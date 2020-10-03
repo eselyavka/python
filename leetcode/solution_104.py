03277ee993019038832c04cd3c246cd0b72eaa57
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-10-03 15:44:40 -0700

    revisit #104

--- leetcode/solution_104.py
+++ leetcode/solution_104.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+
 class TreeNode(object):
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
+
 
 class Solution(object):
     def maxDepth(self, root):
@@ -16,22 +18,13 @@ class Solution(object):
         if root is None:
             return 0
 
-        depth_left = 0
-        depth_right = 0
-
         depth_left = 1 + self.maxDepth(root.left)
         depth_right = 1 + self.maxDepth(root.right)
 
-        return depth_left if depth_left > depth_right else depth_right
+        return max(depth_left, depth_right)
 
-def print_preorder(root):
-    if root:
-        print(root.val),
-        print_preorder(root.left)
-        print_preorder(root.right)
 
 if __name__ == '__main__':
-
     root = TreeNode(1)
     root.left = TreeNode(0)
     root.right = TreeNode(2)
@@ -45,10 +38,6 @@ if __name__ == '__main__':
     root2.left.right.right = None
 
     depth1 = Solution().maxDepth(root)
+    assert depth1 == 2
     depth2 = Solution().maxDepth(root2)
-
-    print depth1, depth2
-
-    print_preorder(root)
-    print
-    print_preorder(root2)
+    assert depth2 == 4
