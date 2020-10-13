03ea8b80f1d1ccd8557c8ea413a8605799876609
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-10-13 15:39:26 -0700

    revisit #921

--- leetcode/solution_921.py
+++ leetcode/solution_921.py
@@ -26,11 +26,38 @@ class Solution(object):
         return unbalanced + len(open_)
 
 
+class Solution2(object):
+    def minAddToMakeValid(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        if not S:
+            return 0
+
+        balance, res = 0, 0
+
+        for c in S:
+            if c == '(':
+                balance += 1
+            else:
+                balance -= 1
+
+            if balance < 0:
+                res += abs(balance)
+                balance = 0
+
+        return res + balance
+
+
 class TestSolution(unittest.TestCase):
 
     def test_minAddToMakeValid(self):
         solution = Solution()
         self.assertEqual(solution.minAddToMakeValid('())'), 1)
+
+        solution2 = Solution2()
+        self.assertEqual(solution2.minAddToMakeValid('())'), 1)
 
 
 if __name__ == '__main__':
