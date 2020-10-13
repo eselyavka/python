bedcbbe5225f943278acd011e8975831bdb094e0
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-10-13 15:26:40 -0700

    assigment #316 and #1081

---
+++ leetcode/solution_1081.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import unittest
+from collections import Counter
+
+
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        counter = Counter(s)
+
+        for c in s:
+            if c in res:
+                counter[c] -= 1
+                continue
+            while res and res[-1] > c and counter[res[-1]] > 0:
+                res.pop()
+
+            res.append(c)
+            counter[c] -= 1
+
+        return ''.join(res)
+
+
+class TestSolution(unittest.TestCase):
+    def test_smallestSubsequence(self):
+        solution = Solution()
+        self.assertEqual(solution.smallestSubsequence('leetcode'), 'letcod')
+
+
+if __name__ == '__main__':
+    unittest.main()
