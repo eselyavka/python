1c6ebb7e662a24ca760e7c78d08fc3f873211920
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-11-10 13:50:20 -0800

    revisit #448

--- leetcode/solution_448.py
+++ leetcode/solution_448.py
@@ -2,6 +2,30 @@
 
 import unittest
 
+
+class Solution2(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+
+        for i in range(n):
+            idx = abs(nums[i]) - 1
+
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+
+        res = []
+
+        for i in range(n):
+            if nums[i] > 0:
+                res.append(i + 1)
+
+        return res
+
+
 class Solution(object):
     def findDisappearedNumbers(self, nums):
         """
@@ -15,13 +39,21 @@ class Solution(object):
                 res.append(i)
 
         return res
+
+
 class TestSolution(unittest.TestCase):
 
     def test_findDisappearedNumbers(self):
         solution = Solution()
 
         self.assertEqual(solution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]), [5, 6])
-        self.assertEqual(solution.findDisappearedNumbers([1, 3]), [2])
+        self.assertEqual(solution.findDisappearedNumbers([1, 3, 3]), [2])
+
+        solution2 = Solution2()
+
+        self.assertEqual(solution2.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]), [5, 6])
+        self.assertEqual(solution2.findDisappearedNumbers([1, 3, 3]), [2])
+
 
 if __name__ == '__main__':
     unittest.main()
