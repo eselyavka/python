1e834e42ef6f5a2ce7107b142226778860a8d2ae
Evgenii Seliavka <evg.selyavka@gmail.com>
2020-12-07 20:29:12 -0800

    revisit #287

--- leetcode/solution_287.py
+++ leetcode/solution_287.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def findDuplicate(self, nums):
         """
@@ -18,11 +19,56 @@ class Solution(object):
 
             i += 1
 
+
+class Solution2(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set()
+        res = -1
+        for num in nums:
+            if num in seen:
+                res = num
+            seen.add(num)
+
+        return res
+
+
+class Solution3(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        fast = slow = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return fast
+
+
 class TestSolution(unittest.TestCase):
     def test_findDuplicate(self):
-        solution = Solution()
-        self.assertEqual(solution.findDuplicate([1, 3, 4, 2, 2]), 2)
-        self.assertEqual(solution.findDuplicate([3, 1, 3, 4, 2]), 3)
+        solution1 = Solution()
+        solution2 = Solution2()
+        solution3 = Solution3()
+        dups1 = [1, 3, 4, 2, 2]
+        dups2 = [3, 1, 3, 4, 2]
+        for solution in [solution1, solution2, solution3]:
+            self.assertEqual(solution.findDuplicate(dups1), 2)
+            self.assertEqual(solution.findDuplicate(dups2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
