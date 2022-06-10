0be09d09b0570acc7920bc1991759c1eca648ff9
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-06-10 12:59:59 -0400

    assigment #1570

---
+++ leetcode/solution_1570.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import unittest
+
+class SparseVector:
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.idx_map = {}
+
+        for idx, num in enumerate(nums):
+            if num:
+                self.idx_map[idx] = num
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        """
+        :type vec: 'SparseVector'
+        :rtype: int
+        """
+        res = 0
+        for idx, v1 in self.idx_map.items():
+            v2 = vec.idx_map.get(idx)
+            if v2:
+                res += v1 * v2
+
+        return res
+
+class TestSolution(unittest.TestCase):
+    def test_SparseVector(self):
+        v1 = SparseVector([1, 0, 0, 2, 3])
+        v2 = SparseVector([0, 3, 0, 4, 0])
+        self.assertEqual(v1.dotProduct(v2), 8)
+
+if __name__ == '__main__':
+    unittest.main()
