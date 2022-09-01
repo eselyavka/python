29090226b75b27fddfda436ff56d9a0b283acbaa
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-09-01 12:25:26 -0400

    assigment #15 alternative

--- leetcode/solution_15.py
+++ leetcode/solution_15.py
@@ -30,15 +30,60 @@ class Solution(object):
 
         return [list(x) for x in res]
 
+
+class Solution2(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 3:
+            return [nums] if sum(nums) == 0 else []
+
+        res = set()
+        for i in range(len(nums)-1):
+            s = set()
+            for j in range(i+1, len(nums)):
+                sum_ = -nums[i] - nums[j]
+                if sum_ in s:
+                    res.add(tuple(sorted((nums[i],nums[j],sum_))))
+                s.add(nums[j])
+
+        return [list(x) for x in res]
+
+
+def nestedListEqual(nestedList1, nestedList2):
+    sortedNestedList1 = [sorted(lst) for lst in nestedList1]
+    sortedNestedList2 = [sorted(lst) for lst in nestedList2]
+
+    for lst1 in sortedNestedList1:
+        equal = False
+        for lst2 in sortedNestedList2:
+            if lst1 == lst2:
+                equal = True
+
+        if not equal:
+            return False
+
+    return True
+
 class TestSolution(unittest.TestCase):
 
     def test_threeSum(self):
         solution = Solution()
-        self.assertEqual(solution.threeSum([-1, 0, 1, 2, -1, -4]),
-                         [[-1, -1, 2], [-1, 0, 1]])
+        solution2 = Solution2()
+
+        self.assertTrue(nestedListEqual(solution.threeSum([-1, 0, 1, 2, -1, -4]),
+                         [[-1, -1, 2], [-1, 0, 1]]))
         self.assertEqual(solution.threeSum([1, 1, -2]), [[-2, 1, 1]])
         self.assertEqual(solution.threeSum([0, 0, 0, 0]), [[0, 0, 0]])
         self.assertEqual(solution.threeSum([-2, 0, 0, 2, 2]), [[-2, 0, 2]])
+
+        self.assertTrue(nestedListEqual(solution2.threeSum([-1, 0, 1, 2, -1, -4]),
+                         [[-1, -1, 2], [-1, 0, 1]]))
+        self.assertTrue(nestedListEqual(solution2.threeSum([1, 1, -2]), [[-2, 1, 1]]))
+        self.assertEqual(solution2.threeSum([0, 0, 0, 0]), [[0, 0, 0]])
+        self.assertEqual(solution2.threeSum([-2, 0, 0, 2, 2]), [[-2, 0, 2]])
 
 if __name__ == '__main__':
     unittest.main()
