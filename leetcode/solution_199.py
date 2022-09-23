c81c0eaf02cadb63e817321b259685f43472e83e
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-09-23 13:19:41 -0400

    assigment #199 alternative

--- leetcode/solution_199.py
+++ leetcode/solution_199.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
+from collections import deque, defaultdict
 
 
 class TreeNode(object):
@@ -34,6 +35,37 @@ class Solution(object):
         return res
 
 
+class Solution2(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        q = deque([(root, 0)])
+
+        level_traversals = defaultdict(list)
+
+        while q:
+            node, level = q.popleft()
+            level_traversals[level].append(node.val)
+
+            if node.left:
+                q.append((node.left, level+1))
+
+            if node.right:
+                q.append((node.right, level+1))
+
+        res = []
+
+        for item in sorted(level_traversals.keys()):
+            res.append(level_traversals[item][-1])
+
+        return res
+
+
 class TestSolution(unittest.TestCase):
     def test_rightSideView(self):
         root = TreeNode(1)
@@ -43,8 +75,10 @@ class TestSolution(unittest.TestCase):
         root.right.right = TreeNode(4)
 
         solution = Solution()
+        solution2 = Solution2()
 
         self.assertListEqual(solution.rightSideView(root), [1, 3, 4])
+        self.assertListEqual(solution2.rightSideView(root), [1, 3, 4])
 
 
 if __name__ == '__main__':
