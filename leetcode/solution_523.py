9c5a2c18f3bf98679bfdb9ca72b3a147ad20dec5
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-09-25 19:38:59 -0400

    assigment #523 alternative

--- leetcode/solution_523.py
+++ leetcode/solution_523.py
@@ -10,6 +10,29 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+
+        reminders = {0:-1}
+        total = 0
+
+        for i, n in enumerate(nums):
+            total += n
+            r = total % k
+
+            if r not in reminders:
+                reminders[r] = i
+            elif i - reminders[r] > 1:
+                return True
+
+        return False
+
+
+class Solution2(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
         n = len(nums)
 
         for i in range(n):
@@ -27,6 +50,9 @@ class TestSolution(unittest.TestCase):
     def test_checkSubarraySum(self):
         solution = Solution()
         self.assertTrue(solution.checkSubarraySum([23, 2, 4, 6, 7], 6))
+
+        solution2 = Solution2()
+        self.assertTrue(solution2.checkSubarraySum([23, 2, 4, 6, 7], 6))
 
 
 if __name__ == '__main__':
