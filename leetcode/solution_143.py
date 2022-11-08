4d54c162e823b99ad892e7f2152c8f31bc759bd7
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-11-08 13:18:29 -0500

    assigment #143, alternative

--- leetcode/solution_143.py
+++ leetcode/solution_143.py
@@ -37,6 +37,36 @@ class Solution(object):
 
         res[-1].next = None
 
+
+class Solution2(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: void Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            next_=second.next
+            second.next = prev
+            prev = second
+            second = next_
+
+        first, second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+
 class TestSolution(unittest.TestCase):
 
     def list_traversals(self, head):
@@ -59,6 +89,17 @@ class TestSolution(unittest.TestCase):
         solution.reorderList(node)
 
         self.assertListEqual(self.list_traversals(node), [1, 4, 2, 3])
+
+        solution2 = Solution2()
+        node = ListNode(1)
+        node.next = ListNode(2)
+        node.next.next = ListNode(3)
+        node.next.next.next = ListNode(4)
+        node.next.next.next.next = ListNode(5)
+
+        solution2.reorderList(node)
+
+        self.assertListEqual(self.list_traversals(node), [1, 5, 2, 4, 3])
 
 if __name__ == '__main__':
     unittest.main()
