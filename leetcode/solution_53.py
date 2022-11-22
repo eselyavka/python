0faa8905eec92d7aacd818ccd852a3d9ca16fee2
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-11-22 08:49:07 -0500

    refactor #53

--- leetcode/solution_53.py
+++ leetcode/solution_53.py
@@ -11,13 +11,12 @@ class Solution(object):
         max_so_far = float('-inf')
         max_ending_here = 0
 
-        for i in nums:
-            max_ending_here = max_ending_here + i
+        for num in nums:
+            max_ending_here = max_ending_here + num
 
-            max_so_far = max(max_ending_here, max_so_far)
+            max_so_far = max(max_so_far, max_ending_here)
+            max_ending_here = max(max_ending_here, 0)
 
-            if max_ending_here < 0:
-                max_ending_here = 0
         return max_so_far
 
 class TestSolution(unittest.TestCase):
