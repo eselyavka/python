c6c3b92e50eb7a3e01f26293ebed4bc7200b23ef
Evgenii Seliavka <evg.selyavka@gmail.com>
2022-11-28 13:24:32 -0500

    refactor #5 with dp

--- leetcode/solution_5.py
+++ leetcode/solution_5.py
@@ -9,25 +9,54 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if len(s) == 1:
+        def is_palindrome(s, left, right):
+            while left <= right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        n = len(s)
+        if n == 1:
             return s
 
         if s == s[::-1]:
             return s
 
-        def is_palindrom(s, i, j):
-            substr = s[i:j+1]
-            return (substr, substr == substr[::-1])
+        max_ = 1
+        ans = ""
+        for i in range(n):
+            for j in range(i+1, n):
+                if is_palindrome(s, i, j):
+                    if j-i+1 > max_:
+                        max_ = j-i+1
+                        ans = s[i:j+1]
+        return ans if max_ > 1 else s[0]
 
-        max_= ""
-        for i in range(len(s)):
-            for j in range(len(s)-1, i, -1):
-                if s[i] == s[j]:
-                    res, flag = is_palindrom(s,i,j)
-                    if flag and len(max_) < len(res):
-                        max_ = res
 
-        return max_ if max_ else s[0]
+class Solution2(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        res = ""
+        dp = [None for i in range(n)]
+
+        for j in range(n):
+            for i in range(j+1):
+                if i == j:
+                    dp[i] = True
+                elif j == i+1:
+                    dp[i] = (s[i] == s[j])
+                else:
+                    dp[i] = (dp[i+1] and s[i] == s[j])
+                if dp[i] and j - i + 1 > len(res):
+                    res = s[i:j+1]
+
+        return res
 
 
 class TestSolution(unittest.TestCase):
@@ -35,6 +64,9 @@ class TestSolution(unittest.TestCase):
         solution = Solution()
 
         self.assertEqual(solution.longestPalindrome("babad"), "bab")
+
+        solution2 = Solution2()
+        self.assertEqual(solution2.longestPalindrome("babad"), "bab")
 
 
 if __name__ == '__main__':
