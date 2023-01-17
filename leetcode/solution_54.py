18c24c2ba2b897fec05763b50fd36d5ee6c41b92
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-01-17 15:27:26 -0500

    refactor #54

--- leetcode/solution_54.py
+++ leetcode/solution_54.py
@@ -9,8 +9,9 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
+
         def rec(matrix, res, init_x, init_y, row, col):
-            if init_x > len(matrix)-1 or init_y > len(matrix[0])-1:
+            if init_x > len(matrix) - 1 or init_y > len(matrix[0]) - 1:
                 return
 
             x = init_x
@@ -49,16 +50,51 @@ class Solution(object):
                     res.append(matrix[x][y])
                     matrix[x][y] = float('-inf')
 
-            rec(matrix, res, init_x+1, init_y+1, row-1, col-1)
+            rec(matrix, res, init_x + 1, init_y + 1, row - 1, col - 1)
 
         if not matrix:
             return []
 
         res = []
 
-        rec(matrix, res, 0, 0, len(matrix)-1, len(matrix[0])-1)
+        rec(matrix, res, 0, 0, len(matrix) - 1, len(matrix[0]) - 1)
 
         return res
+
+
+class Solution2(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ans = []
+        left, right = 0, n
+        top, bottom = 0, m
+        while left < right and top < bottom:
+            for i in range(left, right):
+                ans.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom):
+                ans.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            for i in range(right - 1, left - 1, -1):
+                ans.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            for i in range(bottom - 1, top - 1, -1):
+                ans.append(matrix[i][left])
+            left += 1
+
+        return ans
 
 
 class TestSolution(unittest.TestCase):
@@ -67,6 +103,10 @@ class TestSolution(unittest.TestCase):
         self.assertListEqual(solution.spiralOrder([[1, 2, 3],
                                                    [4, 5, 6],
                                                    [7, 8, 9]]), [1, 2, 3, 6, 9, 8, 7, 4, 5])
+        solution2 = Solution2()
+        self.assertListEqual(solution2.spiralOrder([[1, 2, 3],
+                                                    [4, 5, 6],
+                                                    [7, 8, 9]]), [1, 2, 3, 6, 9, 8, 7, 4, 5])
 
 
 if __name__ == '__main__':
