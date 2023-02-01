55ec0eb8c6e867ce85a450557ba270dc8f70d298
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-02-01 11:38:40 -0500

    refactor #739

--- leetcode/solution_739.py
+++ leetcode/solution_739.py
@@ -2,33 +2,34 @@
 
 import unittest
 
+
 class Solution(object):
     def dailyTemperatures(self, temperatures):
         """
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        size = len(temperatures)
+        n = len(temperatures)
 
-        s = []
-        res = [0] * size
-        i = size - 1
+        stack = [0]
+        ans = [0] * n
 
-        while i >= 0:
-            while s and temperatures[i] >= temperatures[s[-1]]:
-                s.pop()
-            if s:
-                res[i] = s[-1] - i
-            s.append(i)
-            i -= 1
+        for i in range(1, n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                idx = stack.pop()
+                ans[idx] = i - idx
 
-        return res
+            stack.append(i)
+
+        return ans
+
 
 class TestSolution(unittest.TestCase):
     def test_dailyTemperatures(self):
         solution = Solution()
         self.assertEqual(solution.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]),
                          [1, 1, 4, 2, 1, 1, 0, 0])
+
 
 if __name__ == '__main__':
     unittest.main()
