e97c2757c81b4eebb3a9f73c1e74374ecbb4e7cd
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-02-13 13:26:46 -0500

    assigment #78

--- leetcode/solution_78.py
+++ leetcode/solution_78.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def subsets(self, nums):
         """
@@ -9,25 +10,53 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        size = 2**len(nums)
+        size = 2 ** len(nums)
         res = []
         for i in range(size):
             tmp = []
             for j in range(len(nums)):
-                if i & (1<<j):
+                if i & (1 << j):
                     tmp.append(nums[j])
             res.append(tmp)
 
         return res
 
+
+class Solution2(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        def rec(nums, idx, subset, acc):
+            acc.append(subset[:])
+            for i in range(idx, len(nums)):
+                subset.append(nums[i])
+                rec(nums, i + 1, subset, acc)
+                subset.pop()
+
+        ans = []
+        subset = []
+        rec(nums, 0, subset, ans)
+
+        return ans
+
+
 class TestSolution(unittest.TestCase):
 
     def test_subsets(self):
         solution = Solution()
-        self.assertEqual(solution.subsets([1, 2, 3]), [[], [1], [2], [1, 2], [3],
-                                                       [1, 3], [2, 3], [1, 2, 3]])
-        self.assertEqual(solution.subsets([1, 2]), [[], [1], [2], [1, 2]])
-        self.assertEqual(solution.subsets([0]), [[], [0]])
+        self.assertListEqual(sorted(solution.subsets([1, 2, 3])), sorted([[], [1], [2], [1, 2], [3],
+                                                                          [1, 3], [2, 3], [1, 2, 3]]))
+        self.assertListEqual(sorted(solution.subsets([1, 2])), sorted([[], [1], [2], [1, 2]]))
+        self.assertListEqual(sorted(solution.subsets([0])), sorted([[], [0]]))
+        solution2 = Solution2()
+        self.assertListEqual(sorted(solution2.subsets([1, 2, 3])), sorted([[], [1], [2], [1, 2], [3],
+                                                                           [1, 3], [2, 3], [1, 2, 3]]))
+        self.assertListEqual(sorted(solution2.subsets([1, 2])), sorted([[], [1], [2], [1, 2]]))
+        self.assertListEqual(sorted(solution2.subsets([0])), sorted([[], [0]]))
+
 
 if __name__ == '__main__':
     unittest.main()
