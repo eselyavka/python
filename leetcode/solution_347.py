b877df7e295cad9feb78da4b8be9f00f5dc940e1
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-02-17 16:34:23 -0500

    refactor #347

--- leetcode/solution_347.py
+++ leetcode/solution_347.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import unittest
-from collections import defaultdict
+from collections import defaultdict, Counter
 import heapq
+
 
 class Solution(object):
     def topKFrequent(self, nums, k):
@@ -22,7 +23,30 @@ class Solution(object):
             return [x[0] for x in heapq.nlargest(k, res, key=lambda x: x[1])]
 
         res.sort(key=lambda x: x[1])
-        return [x[0] for x in res[:-(k+1):-1]]
+        return [x[0] for x in res[:-(k + 1):-1]]
+
+
+class Solution2(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        return res
 
 
 class TestSolution(unittest.TestCase):
@@ -34,6 +58,11 @@ class TestSolution(unittest.TestCase):
 
         self.assertEqual(solution.topKFrequent(arr, 2),
                          [1, 2])
+
+        solution2 = Solution2()
+        self.assertEqual(sorted(solution2.topKFrequent(arr, 2)),
+                         [1, 2])
+
 
 if __name__ == '__main__':
     unittest.main()
