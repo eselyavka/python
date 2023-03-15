96649fa2bd4603e90b895ae0c4bc99661633b3dd
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-03-15 12:35:09 -0400

    refactor #215

--- leetcode/solution_215.py
+++ leetcode/solution_215.py
@@ -30,6 +30,35 @@ class Solution2(object):
         return heapq.nlargest(k, nums)[-1]
 
 
+class Solution3(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        def quickSelect(l, r, k):
+            pivot, p = nums[r], l
+
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if k < p:
+                return quickSelect(l, p - 1, k)
+
+            if k > p:
+                return quickSelect(p + 1, r, k)
+
+            return nums[p]
+
+        return quickSelect(0, len(nums) - 1, len(nums) - k)
+
+
 class TestSolution(unittest.TestCase):
 
     def test_findKthLargest(self):
@@ -39,6 +68,9 @@ class TestSolution(unittest.TestCase):
 
         solution2 = Solution2()
         self.assertEqual(solution2.findKthLargest(nums, 2), 5)
+
+        solution3 = Solution3()
+        self.assertEqual(solution3.findKthLargest(nums, 2), 5)
 
 
 if __name__ == '__main__':
