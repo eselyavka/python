3e5521a30b6d29915265bedcb5dc8efbc93e697f
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-03-27 12:32:02 -0400

    refactor #46

--- leetcode/solution_46.py
+++ leetcode/solution_46.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def permute(self, nums):
         """
@@ -16,24 +17,58 @@ class Solution(object):
             else:
                 for k in range(i, j + 1):
                     nums[i], nums[k] = nums[k], nums[i]
-                    rec(i+1, j)
+                    rec(i + 1, j)
                     nums[i], nums[k] = nums[k], nums[i]
 
         rec(0, len(nums) - 1)
 
         return res
 
+
+class Solution2(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ans = []
+
+        if len(nums) == 1:
+            return [[nums[0]]]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+
+            ans.extend(perms)
+
+            nums.append(n)
+
+        return ans
+
+
 class TestSolution(unittest.TestCase):
 
     def test_permutate(self):
         nums = [1, 2, 3]
         solution = Solution()
-        self.assertEqual(solution.permute(nums), [[1, 2, 3],
-                                                  [1, 3, 2],
-                                                  [2, 1, 3],
-                                                  [2, 3, 1],
-                                                  [3, 2, 1],
-                                                  [3, 1, 2]])
+        self.assertListEqual(sorted(solution.permute(nums)), sorted([[1, 2, 3],
+                                                                     [1, 3, 2],
+                                                                     [2, 1, 3],
+                                                                     [2, 3, 1],
+                                                                     [3, 2, 1],
+                                                                     [3, 1, 2]]))
+        solution2 = Solution2()
+        self.assertListEqual(sorted(solution2.permute(nums)), sorted([[1, 2, 3],
+                                                                      [1, 3, 2],
+                                                                      [2, 1, 3],
+                                                                      [2, 3, 1],
+                                                                      [3, 2, 1],
+                                                                      [3, 1, 2]]))
+
 
 if __name__ == '__main__':
     unittest.main()
