e574b04d3601b31f8fa60e37d6d0975171a584a0
Evgenii Seliavka <evg.selyavka@gmail.com>
2023-12-15 12:07:28 -0500

    assigment #1436

---
+++ leetcode/solution_1436.py
@@ -0,0 +1,35 @@
+import unittest
+from collections import defaultdict
+
+
+class Solution(object):
+    def destCity(self, paths):
+        """
+        :type paths: List[List[str]]
+        :rtype: str
+        """
+        G = defaultdict(list)
+
+        for start, end in paths:
+            G[start] = end
+
+        ans = paths[0][0]
+
+        while True:
+            if ans in G:
+                ans = G[ans]
+                continue
+            return ans
+
+
+class TestSolution(unittest.TestCase):
+    def test_destCity(self):
+        solution = Solution()
+        self.assertEqual(solution.destCity([["London", "New York"],
+                                            ["New York", "Lima"],
+                                            ["Lima", "Sao Paulo"]]),
+                         "Sao Paulo")
+
+
+if __name__ == '__main__':
+    unittest.main()
