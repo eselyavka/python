6883877a5e7e3baaa9e8252f9576b5fc10535ff0
Evgenii Seliavka <evg.selyavka@gmail.com>
2024-11-13 12:18:08 -0500

    refactor #108

--- leetcode/solutions_00000/solution_00108.py
+++ leetcode/solutions_00000/solution_00108.py
@@ -2,27 +2,33 @@
 
 import unittest
 
+
 class TreeNode(object):
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
 
+
 class Solution(object):
     def sortedArrayToBST(self, nums):
         """
         :type nums: List[int]
-        :rtype: TreeNode
+        :rtype: Optional[TreeNode]
         """
 
-        if nums:
-            mid = len(nums) // 2
-            root = TreeNode(nums[mid])
-            root.left = self.sortedArrayToBST(nums[:mid])
-            root.right = self.sortedArrayToBST(nums[mid+1:])
-            return root
+        def sorted_list_to_bst(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = sorted_list_to_bst(left, mid - 1)
+            node.right = sorted_list_to_bst(mid + 1, right)
 
-        return None
+            return node
+
+        return sorted_list_to_bst(0, len(nums) - 1)
+
 
 class TestSolution(unittest.TestCase):
     def _helper(self, root):
@@ -49,10 +55,11 @@ class TestSolution(unittest.TestCase):
         return arr
 
     def test_sortedArrayToBST(self):
-        solution = Solution()
         arr = [-10, -3, 0, 5, 9]
+        solution = Solution()
         self.assertListEqual(self._helper(solution.sortedArrayToBST(arr)),
-                             [0, -3, 9, -10, 5])
+                             [0, -10, 5, -3, 9])
+
 
 if __name__ == '__main__':
     unittest.main()
