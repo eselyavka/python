04d3147d0508d39211c43b5899f146f66fe6fa98
Evgenii Seliavka <evg.selyavka@gmail.com>
2024-12-02 12:48:35 -0500

    refactor #994

--- leetcode/solutions_00900/solution_00994.py
+++ leetcode/solutions_00900/solution_00994.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 
 class Solution(object):
@@ -44,10 +45,56 @@ class Solution(object):
         return ts - 2
 
 
+class Solution2(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+
+        queue = deque()
+        fresh_oranges = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                if grid[i][j] == 1:
+                    fresh_oranges += 1
+
+        ans = -1
+        seen = [[False] * n for _ in range(m)]
+        for q in queue:
+            seen[q[0]][q[1]] = True
+
+        queue.append((-1, -1))
+
+        while queue:
+            cnt = len(queue)
+            while cnt > 0:
+                x, y = queue.popleft()
+                for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    dx = x + d[0]
+                    dy = y + d[1]
+                    if 0 <= dx < m and 0 <= dy < n and seen[dx][dy] == False:
+                        seen[dx][dy] = True
+                        if grid[dx][dy] == 1:
+                            grid[dx][dy] = 2
+                            queue.append((dx, dy))
+                            fresh_oranges -= 1
+                cnt -= 1
+            ans += 1
+
+        return ans if fresh_oranges == 0 else -1
+
+
 class TestSolution(unittest.TestCase):
     def test_orangesRotting(self):
         solution = Solution()
         self.assertEqual(solution.orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]), 4)
+        solution2 = Solution2()
+        self.assertEqual(solution2.orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]), 4)
 
 
 if __name__ == '__main__':
