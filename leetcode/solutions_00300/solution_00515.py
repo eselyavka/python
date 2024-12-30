236ce632349142eff71b4c1c92d61ecb33bbec32
Evgenii Seliavka <evg.selyavka@gmail.com>
2024-12-30 10:40:42 -0500

    refactor #515

--- leetcode/solutions_00300/solution_00515.py
+++ leetcode/solutions_00300/solution_00515.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
+from collections import deque
+
 
 class TreeNode(object):
     def __init__(self, val):
@@ -8,36 +10,35 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
 class Solution(object):
-    def height(self, root):
-        if not root:
-            return 0
-        else:
-            lheight = self.height(root.left)
-            rheight = self.height(root.right)
-
-            if lheight > rheight:
-                return lheight + 1
-            return rheight + 1
-
-    def traverseGivenLevel(self, root, level):
-        if root:
-            if level == 1:
-                return root.val
-            elif level > 1:
-                return max(self.traverseGivenLevel(root.right, level-1),
-                           self.traverseGivenLevel(root.left, level-1))
-
     def largestValues(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: List[int]
         """
-        res = list()
-        for level in range(1, self.height(root) + 1):
-            res.append(self.traverseGivenLevel(root, level))
+        if not root:
+            return []
 
-        return res
+        q = deque()
+        q.append(root)
+
+        ans = []
+        while q:
+            row_max = float("-inf")
+            it = len(q)
+            while it > 0:
+                node = q.popleft()
+                row_max = max(row_max, node.val)
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+                it -= 1
+            ans.append(row_max)
+
+        return ans
+
 
 class TestSolution(unittest.TestCase):
 
@@ -52,6 +53,7 @@ class TestSolution(unittest.TestCase):
 
         solution = Solution()
         self.assertEqual(solution.largestValues(root), [1, 3, 9])
+
 
 if __name__ == '__main__':
     unittest.main()
