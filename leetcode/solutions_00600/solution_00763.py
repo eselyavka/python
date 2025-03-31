d5920c455771466bad6f7709f8a7b5e1d5722e0e
Evgenii Seliavka <evg.selyavka@gmail.com>
2025-03-31 14:43:47 -0400

    refactor #763

--- leetcode/solutions_00600/solution_00763.py
+++ leetcode/solutions_00600/solution_00763.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
+from collections import defaultdict
+
 
 class Solution(object):
     def partitionLabels(self, S):
@@ -12,15 +14,48 @@ class Solution(object):
         res = []
         for i, letter in enumerate(S):
             if i > r:
-                res.append((r - l)+1)
+                res.append((r - l) + 1)
                 l = i
 
             r = max(r, S.rindex(letter))
 
             if i == len(S) - 1:
-                res.append((r-l) + 1)
+                res.append((r - l) + 1)
 
         return res
+
+
+class Solution2(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        last = defaultdict(list)
+
+        for i, c in enumerate(s):
+            last[c].append(i)
+
+        intervals = list(last.values())
+        intervals.sort(key=lambda t: t[0])
+
+        merged_intervales = []
+        i = 0
+        start, end = intervals[0][0], intervals[0][-1]
+        for i in range(1, len(intervals)):
+            curr_start, curr_end = intervals[i][0], intervals[i][-1]
+            if curr_start <= end:
+                end = max(end, curr_end)
+            else:
+                merged_intervales.append([start, end])
+                start, end = curr_start, curr_end
+
+        merged_intervales.append([start, end])
+
+        ans = [t[-1] - t[0] + 1 for t in merged_intervales]
+
+        return ans
+
 
 class TestSolution(unittest.TestCase):
 
@@ -31,6 +66,14 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.partitionLabels('b'), [1])
         self.assertEqual(solution.partitionLabels('abaccbdeffed'), [6, 6])
         self.assertEqual(solution.partitionLabels('eaaaabaaec'), [9, 1])
+
+        solution2 = Solution2()
+        self.assertEqual(solution2.partitionLabels('ababcbacadefegdehijhklij'), [9, 7, 8])
+        self.assertEqual(solution2.partitionLabels('aaaaaa'), [6])
+        self.assertEqual(solution2.partitionLabels('b'), [1])
+        self.assertEqual(solution2.partitionLabels('abaccbdeffed'), [6, 6])
+        self.assertEqual(solution2.partitionLabels('eaaaabaaec'), [9, 1])
+
 
 if __name__ == '__main__':
     unittest.main()
