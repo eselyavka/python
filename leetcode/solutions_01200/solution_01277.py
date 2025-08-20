1f8c57ef4aacf6c3f37cdc16fcaaa7531885e0fa
Evgenii Seliavka <evg.selyavka@gmail.com>
2025-08-20 16:50:53 -0400

    refactor #1277

--- leetcode/solutions_01200/solution_01277.py
+++ leetcode/solutions_01200/solution_01277.py
@@ -12,14 +12,16 @@ class Solution(object):
         row = len(matrix)
         col = len(matrix[0])
 
-        dp = [[0 for _ in range(col+1)] for _ in range(row+1)]
+        dp = [[0 for _ in range(col + 1)] for _ in range(row + 1)]
+        ans = 0
 
-        for i in range(1, row+1):
-            for j in range(1, col+1):
-                if matrix[i-1][j-1] == 1:
-                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+        for i in range(1, row + 1):
+            for j in range(1, col + 1):
+                if matrix[i - 1][j - 1] == 1:
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                    ans += dp[i][j]
 
-        return sum([sum(x) for x in dp])
+        return ans
 
 
 class TestSolution(unittest.TestCase):
