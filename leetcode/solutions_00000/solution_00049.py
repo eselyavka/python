c7da717fa035e8815298d73f3fd31c1676a6f254
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-01-07 19:45:09 -0500

    refactor #49

--- leetcode/solutions_00000/solution_00049.py
+++ leetcode/solutions_00000/solution_00049.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
+from collections import defaultdict
+
 
 class Solution(object):
     def groupAnagrams(self, strs):
@@ -16,7 +18,26 @@ class Solution(object):
             except KeyError:
                 d[ss] = [s]
 
-        return d.values()
+        return list(d.values())
+
+
+class Solution2(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        ans = defaultdict(list)
+
+        for s in strs:
+            cnt = [0] * 26
+            for c in s:
+                cnt[ord(c) - ord("a")] += 1
+            ans[tuple(cnt)].append(s)
+
+        return list(ans.values())
+
 
 class TestSolution(unittest.TestCase):
     def test_groupAnagrams(self):
@@ -26,10 +47,21 @@ class TestSolution(unittest.TestCase):
                                                  "tan",
                                                  "ate",
                                                  "nat",
-                                                 "bat"]),
+                                                 "bat"]).sort(),
                          [['tan', 'nat'],
                           ['bat'],
-                          ['eat', 'tea', 'ate']])
+                          ['eat', 'tea', 'ate']].sort())
+        solution2 = Solution2()
+        self.assertEqual(solution2.groupAnagrams(["eat",
+                                                  "tea",
+                                                  "tan",
+                                                  "ate",
+                                                  "nat",
+                                                  "bat"]).sort(),
+                         [['tan', 'nat'],
+                          ['bat'],
+                          ['eat', 'tea', 'ate']].sort())
+
 
 if __name__ == '__main__':
     unittest.main()
