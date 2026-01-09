94480c18df8cd7294657754731344179c0491f97
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-01-08 19:18:13 -0500

    refactor #3

--- leetcode/solutions_00000/solution_00003.py
+++ leetcode/solutions_00000/solution_00003.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -50,11 +51,36 @@ class Solution2(object):
         return max(res, buf)
 
 
+class Solution3(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+
+        last = {}
+        l = 0
+        ans = 0
+
+        for r, ch in enumerate(s):
+            if ch in last:
+                l = max(l, last[ch] + 1)
+
+            last[ch] = r
+
+            ans = max(ans, r - l + 1)
+
+        return ans
+
+
 class TestSolution(unittest.TestCase):
 
     def test_lengthOfLongestSubstring(self):
         solution = Solution()
         solution2 = Solution2()
+        solution3 = Solution3()
 
         self.assertEqual(solution.lengthOfLongestSubstring("abcabcbb"), 3)
         self.assertEqual(solution.lengthOfLongestSubstring("bbbbb"), 1)
@@ -65,6 +91,12 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution2.lengthOfLongestSubstring("bbbbb"), 1)
         self.assertEqual(solution2.lengthOfLongestSubstring("pwwkew"), 3)
         self.assertEqual(solution2.lengthOfLongestSubstring("dvdf"), 3)
+
+        self.assertEqual(solution3.lengthOfLongestSubstring("abcabcbb"), 3)
+        self.assertEqual(solution3.lengthOfLongestSubstring("bbbbb"), 1)
+        self.assertEqual(solution3.lengthOfLongestSubstring("pwwkew"), 3)
+        self.assertEqual(solution3.lengthOfLongestSubstring("dvdf"), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
