f5da022597cc154d3d6004650ec1828d37326ee3
Evgenii Seliavka <evg.selyavka@gmail.com>
2026-02-06 11:42:23 -0500

    refactor #202

--- leetcode/solutions_00000/solution_00202.py
+++ leetcode/solutions_00000/solution_00202.py
@@ -23,7 +23,7 @@ class Solution(object):
         seen = set([n])
 
         def rec(mas):
-            n = sum([x**2 for x in mas])
+            n = sum([x ** 2 for x in mas])
             if n == 1:
                 return True
             if n in seen:
@@ -36,10 +36,42 @@ class Solution(object):
         return rec(self.number_to_array(n))
 
 
+class Solution2(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        def next_num(x):
+            s = 0
+            while x:
+                x, d = divmod(x, 10)
+                s += d * d
+            return s
+
+        slow = n
+        fast = next_num(n)
+
+        while fast != 1 and slow != fast:
+            slow = next_num(slow)
+            fast = next_num(next_num(fast))
+
+        return fast == 1
+
+
 class TestSolution(unittest.TestCase):
 
     def test_isHappy(self):
         solution = Solution()
+
+        self.assertTrue(solution.isHappy(19))
+        self.assertTrue(solution.isHappy(7))
+        self.assertFalse(solution.isHappy(5))
+        self.assertFalse(solution.isHappy(11))
+        self.assertFalse(solution.isHappy(78))
+
+        solution = Solution2()
 
         self.assertTrue(solution.isHappy(19))
         self.assertTrue(solution.isHappy(7))
